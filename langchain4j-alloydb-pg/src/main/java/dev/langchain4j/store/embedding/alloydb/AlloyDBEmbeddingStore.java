c41024d0e7f4e00881d521f69ecce01d1a6e89f8
oreduardo <oreduardo@google.com>
2025-01-14 23:32:40 +0000

    add missing columns to engine, remove overwriteExisting

--- langchain4j-alloydb-pg/src/main/java/dev/langchain4j/store/embedding/alloydb/AlloyDBEmbeddingStore.java
+++ langchain4j-alloydb-pg/src/main/java/dev/langchain4j/store/embedding/alloydb/AlloyDBEmbeddingStore.java
@@ -6,8 +6,6 @@ import java.util.logging.Logger;
 
 import dev.langchain4j.data.embedding.Embedding;
 import dev.langchain4j.data.segment.TextSegment;
-import static dev.langchain4j.internal.ValidationUtils.ensureNotBlank;
-import static dev.langchain4j.internal.ValidationUtils.ensureNotNull;
 import dev.langchain4j.store.embedding.EmbeddingSearchRequest;
 import dev.langchain4j.store.embedding.EmbeddingSearchResult;
 import dev.langchain4j.store.embedding.EmbeddingStore;
@@ -17,11 +15,13 @@ public class AlloyDBEmbeddingStore implements EmbeddingStore<TextSegment> {
     private static final Logger log = Logger.getLogger(AlloyDBEmbeddingStore.class.getName());
     private final AlloyDBEngine engine;
     private final String tableName;
+    private String schemaName;
     private String contentColumn;
     private String embeddingColumn;
+    private String idColumn;
     private List<String> metadataColumns;
+    private String metadataJsonColumn;
     private List<String> ignoreMetadataColumns;
-    private Boolean overwriteExisting;
     // change to QueryOptions class when implemented
     private List<String> queryOptions;
 
@@ -30,27 +30,31 @@ public class AlloyDBEmbeddingStore implements EmbeddingStore<TextSegment> {
      *
      * @param engine The connection object to use
      * @param tableName The name of the table (no default, user must specify)
+     * @param schemaName (Optional, Default: "public") The schema name
      * @param contentColumn (Optional, Default: “content”) Column that represent
      * a Document’s page content
      * @param embeddingColumn (Optional, Default: “embedding”) Column for
      * embedding vectors. The embedding is generated from the document value
+     * @param idColumn (Optional, Default: "langchain_id") Column to store ids.
      * @param metadataColumns (Optional) Column(s) that represent a document’s
      * metadata
+     * @param metadataJsonColumn (Optional, Default: "langchain_metadata") The
+     * column to store extra metadata in JSON format.
      * @param ignoreMetadataColumns (Optional) Column(s) to ignore in
      * pre-existing tables for a document’s
-     * @param overwriteExisting (Optional, Default: False) Boolean argument for
-     * truncating table before insertion
      * @param queryOptions (Optional) QueryOptions class with vector search
      * parameters
      */
-    public AlloyDBEmbeddingStore(AlloyDBEngine engine, String tableName, String contentColumn, String embeddingColumn, List<String> metadataColumns, List<String> ignoreMetadataColumns, Boolean overwriteExisting, List<String> queryOptions) {
-        this.engine = ensureNotNull(engine, "engine");
-        this.tableName = ensureNotBlank(tableName, "tableName");
-        this.contentColumn = contentColumn.isEmptycontentColumn;
+    public AlloyDBEmbeddingStore(AlloyDBEngine engine, String tableName, String schemaName, String contentColumn, String embeddingColumn, String idColumn, List<String> metadataColumns, String metadataJsonColumn, List<String> ignoreMetadataColumns, List<String> queryOptions) {
+        this.engine = engine;
+        this.tableName = tableName;
+        this.schemaName = schemaName;
+        this.contentColumn = contentColumn;
         this.embeddingColumn = embeddingColumn;
+        this.idColumn = idColumn;
         this.metadataColumns = metadataColumns;
+        this.metadataJsonColumn = metadataJsonColumn;
         this.ignoreMetadataColumns = ignoreMetadataColumns;
-        this.overwriteExisting = overwriteExisting;
         this.queryOptions = queryOptions;
     }
 
@@ -88,18 +92,19 @@ public class AlloyDBEmbeddingStore implements EmbeddingStore<TextSegment> {
 
         private AlloyDBEngine engine;
         private String tableName;
+        private String schemaName;
         private String contentColumn;
         private String embeddingColumn;
+        private String idColumn;
         private List<String> metadataColumns;
+        private String metadataJsonColumn;
         private List<String> ignoreMetadataColumns;
-        private Boolean overwriteExisting;
         // change to QueryOptions class when implemented
         private List<String> queryOptions;
 
         public Builder() {
             this.contentColumn = "content";
             this.embeddingColumn = "embedding";
-            this.overwrite_existing = false;
         }
 
         public Builder engine(AlloyDBEngine engine) {
@@ -109,6 +114,11 @@ public class AlloyDBEmbeddingStore implements EmbeddingStore<TextSegment> {
 
         public Builder tableName(String tableName) {
             this.tableName = tableName;
+            return this;
+        }
+
+        public Builder schemaName(String schemaName) {
+            this.schemaName = schemaName;
             return this;
         }
 
@@ -122,8 +132,18 @@ public class AlloyDBEmbeddingStore implements EmbeddingStore<TextSegment> {
             return this;
         }
 
+        public Builder idColumn(String idColumn) {
+            this.idColumn = idColumn;
+            return this;
+        }
+
         public Builder metadataColumns(List<String> metadataColumns) {
             this.metadataColumns = metadataColumns;
+            return this;
+        }
+
+        public Builder metadataJsonColumn(String metadataJsonColumn) {
+            this.metadataJsonColumn = metadataJsonColumn;
             return this;
         }
 
@@ -132,18 +152,13 @@ public class AlloyDBEmbeddingStore implements EmbeddingStore<TextSegment> {
             return this;
         }
 
-        public Builder overwriteExisting(Boolean overwriteExisting) {
-            this.overwriteExisting = overwriteExisting;
-            return this;
-        }
-
         public Builder queryOptions(List<String> queryOptions) {
             this.queryOptions = queryOptions;
             return this;
         }
 
-        public AlloyDBEngine build() {
-            return new AlloyDBEmbeddingStore(engine, tableName, contentColumn, embeddingColumn, metadataColumns, ignoreMetadataColumns, overwriteExisting, queryOptions);
+        public AlloyDBEmbeddingStore build() {
+            return new AlloyDBEmbeddingStore(engine, tableName, schemaName, contentColumn, embeddingColumn, idColumn, metadataColumns, metadataJsonColumn, ignoreMetadataColumns, queryOptions);
         }
     }
 
