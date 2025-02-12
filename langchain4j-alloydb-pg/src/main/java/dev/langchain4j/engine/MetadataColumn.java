e4aa319127dc6d01ecc03ac6a69f85772fe65eb7
oreduardo <oreduardo@google.com>
2025-02-12 18:20:36 +0000

    implement embedding store add methods

--- langchain4j-alloydb-pg/src/main/java/dev/langchain4j/engine/MetadataColumn.java
+++ langchain4j-alloydb-pg/src/main/java/dev/langchain4j/engine/MetadataColumn.java
@@ -16,4 +16,8 @@ public class MetadataColumn {
         return String.format("\"%s\" %s %s", name, type, nullable ? "" : "NOT NULL");
     }
 
+    public String getName() {
+        return name;
+    }
+
 }
