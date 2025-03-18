b334baaed1ea2e1e87d7027f76990771987c0264
Averi Kitsch <akitsch@google.com>
2025-03-17 17:23:30 -0700

    fix

--- langchain4j-alloydb-pg/src/test/java/dev/langchain4j/engine/AlloyDBEngineIT.java
+++ langchain4j-alloydb-pg/src/test/java/dev/langchain4j/engine/AlloyDBEngineIT.java
@@ -17,6 +17,8 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 @Testcontainers
